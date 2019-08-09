0950e103f11ed0a6ce75614c48679cdf0af9b424
emerfan <emerfanning@MacBook-Pro.fritz.box>
2019-08-09 21:33:20 +0100

    code complete

--- src/main/java/com/emer/api/service/InvoiceService.java
+++ src/main/java/com/emer/api/service/InvoiceService.java
@@ -6,9 +6,10 @@ import java.util.Optional;
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 
+import com.emer.api.exception.InvalidRequestException;
 import com.emer.api.model.DateSearch;
 import com.emer.api.model.Invoice;
-import com.emer.api.model.InvoiceRequest;
+import com.emer.api.model.NewInvoiceRequest;
 import com.itextpdf.text.DocumentException;
 /**
  * 
@@ -45,7 +46,8 @@ public interface InvoiceService {
 	 * @throws DocumentException
 	 * @throws AddressException
 	 * @throws MessagingException
+	 * @throws InvalidRequestException 
 	 */
-	Invoice saveInvoice(InvoiceRequest newInvoiceRequest)
-				throws IOException, DocumentException, AddressException, MessagingException;
+	Invoice saveInvoice(NewInvoiceRequest newInvoiceRequest)
+				throws IOException, DocumentException, AddressException, MessagingException, InvalidRequestException;
 }
